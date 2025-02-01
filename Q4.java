c5e2e3f350b48e6b70de2538aacdace99b18c404
Yenuli Obapitiya <146210226+yenuliobapitiya@users.noreply.github.com>
2025-02-01 13:18:12 +0530

    question 4

---
+++ Q4.java
@@ -0,0 +1,15 @@
+public class Q4 {
+    public static void main(String[] args) {
+        System.out.println("Hello, java");
+        System.out.println("A");
+        System.out.println(1234);
+        System.out.println(-1234);
+        System.out.println(1.25);
+        System.out.println(0.055);
+        System.out.println(-0.055);
+        System.out.println('A');
+        System.out.println('6');
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
