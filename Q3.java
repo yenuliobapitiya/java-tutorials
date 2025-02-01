7f6fb82c9706d46301ec613d3924d9838774c86b
Yenuli Obapitiya <146210226+yenuliobapitiya@users.noreply.github.com>
2025-02-01 13:09:49 +0530

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,8 @@
+public class Q3 {
+    public static void main(String[] args) {
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.println("D");
+    }
+}
