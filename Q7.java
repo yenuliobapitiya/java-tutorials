1c72b2276d50de0061c1568e50ea94b3faf2e058
Yenuli Obapitiya <146210226+yenuliobapitiya@users.noreply.github.com>
2025-02-01 18:33:21 +0530

    Q7 and Q8

---
+++ Q7.java
@@ -0,0 +1,15 @@
+public class Q7 {
+    public static void main(String[] args) {
+        int x;
+        x = 100;
+        System.out.println(x);
+    }
+}
+
+class Q8{
+    public static void main(String[] args) {
+        int x;
+        x = 100;
+        System.out.println("x");
+    }
+}
