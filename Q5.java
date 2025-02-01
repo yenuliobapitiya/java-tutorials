25a541e7fd603e90ff05a9829bc16b654f586f48
Yenuli Obapitiya <146210226+yenuliobapitiya@users.noreply.github.com>
2025-02-01 13:22:25 +0530

    Add files via upload

---
+++ Q5.java
@@ -0,0 +1,9 @@
+public class Q5 {
+    public static void main(String[] args) {
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println();
+        System.out.println("C");
+        System.out.println("D");
+    }
+}
