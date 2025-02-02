9669bea899daf5aabbdb024e8dfbbeeef5c67741
Yenuli Obapitiya <146210226+yenuliobapitiya@users.noreply.github.com>
2025-02-02 20:03:56 +0530

    Q9 & Q10

---
+++ Q9.java
@@ -0,0 +1,21 @@
+public class Q9 {
+    public static void main(String args[]){
+        int x;
+        System.out.println(x);
+    }
+}
+
+//try to fix this error -->
+// To fix this error, initialize the variable x before using it. (int x = 0;)
+
+
+class Q10{
+    public static void main(String args[]){
+        int x;
+        System.out.println(x);
+        x=100;
+    }
+}
+
+//to fix this error -->
+// To fix this error, initialize the variable x before using it. (int x = 100;)
